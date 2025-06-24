db83dcc0e9d34123aca647254f1b706728a94107
Samuel Wall <me@samuelwall.uk>
2025-06-24 21:52:34 +0100

    feat(trim-whitespace): run on save rather than exit insert

--- lua/config/trim-whitespace.lua
+++ lua/config/trim-whitespace.lua
@@ -1,6 +1,5 @@
--- autocmd to trim trailing whitespaces on exit insert mode
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = { "*" },
-  command = "%s/\\s\\+$//e",
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*' },
+  command = '%s/\\s\\+$//e',
+  desc = 'Trim trailing whitespace before saving',
 })
-
