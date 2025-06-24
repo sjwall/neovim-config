072bae5874b758cb99ec1b8b35d4fafbca58a54d
Samuel Wall <me@samuelwall.uk>
2025-06-24 22:01:54 +0100

    refactor(fugitive): use <cmd> instead of :

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,14 +2,14 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Open Git [S]taging' })
-    vim.keymap.set('n', '<leader>gp', ':Git pull<CR>', { desc = 'Git [P]ull' })
-    vim.keymap.set('n', '<leader>gg', ':Git push<CR>', { desc = '[G]it Push' })
+    vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<CR>', { desc = 'Git [P]ull' })
+    vim.keymap.set('n', '<leader>gg', '<cmd>Git push<CR>', { desc = '[G]it Push' })
 
-    vim.keymap.set('n', '<leader>gcm', ':Git checkout main<CR>', { desc = 'Git [C]heckout [M]ain' })
-    vim.keymap.set('n', '<leader>gcb', ':Git checkout -b ', { desc = 'Git [C]heckout new [b]ranch' })
+    vim.keymap.set('n', '<leader>gcm', '<cmd>Git checkout main<CR>', { desc = 'Git [C]heckout [M]ain' })
+    vim.keymap.set('n', '<leader>gcb', '<cmd>Git checkout -b ', { desc = 'Git [C]heckout new [b]ranch' })
 
-    vim.keymap.set('n', '<leader>gmm', ':Git merge main<CR>', { desc = 'Git [M]erge [M]ain' })
+    vim.keymap.set('n', '<leader>gmm', '<cmd>Git merge main<CR>', { desc = 'Git [M]erge [M]ain' })
 
-    vim.keymap.set('n', '<leader>grm', ':Git rebase main<CR>', { desc = 'Git [R]ebase [M]ain' })
+    vim.keymap.set('n', '<leader>grm', '<cmd>Git rebase main<CR>', { desc = 'Git [R]ebase [M]ain' })
   end,
 }
