0911c5c4fa42e080224c190c7bb15abf80eb7335
Samuel Wall <me@samuelwall.uk>
2025-06-30 08:46:50 +0100

    refactor(fugitive): change branch actions keymap to us [b]

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,13 +5,14 @@ return {
     vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<CR>', { desc = 'Git [P]ull' })
     vim.keymap.set('n', '<leader>gg', '<cmd>Git push<CR>', { desc = '[G]it Push' })
 
-    vim.keymap.set('n', '<leader>gcm', '<cmd>Git checkout main<CR>', { desc = 'Git [C]heckout [M]ain' })
-    vim.keymap.set('n', '<leader>gcb', ':Git checkout -b ', { desc = 'Git [C]heckout new [B]ranch' })
+    vim.keymap.set('n', '<leader>gbm', '<cmd>Git checkout main<CR>', { desc = 'Git [B]ranch switch [M]ain' })
+    vim.keymap.set('n', '<leader>gbn', ':Git checkout -b ', { desc = 'Git [B]ranch [N]ew' })
+    vim.keymap.set('n', '<leader>gbs', ':Git checkout ', { desc = 'Git [B]ranch [S]witch' })
     vim.keymap.set(
       'n',
-      '<leader>gcr',
+      '<leader>gbr',
       ':Git checkout -m <C-R>=system("git branch --show-current")<CR><BS>',
-      { desc = 'Git [C]heckout [R]ename branch' }
+      { desc = 'Git [B]ranch [R]ename' }
     )
 
     vim.keymap.set('n', '<leader>gmm', '<cmd>Git merge main<CR>', { desc = 'Git [M]erge [M]ain' })
