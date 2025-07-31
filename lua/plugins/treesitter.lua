b100af40456a4d19a7a36925c32d9d87e547ba17
Samuel Wall <me@samuelwall.uk>
2025-07-31 22:39:16 +0100

    chore(treesitter): upgrade to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  branch = 'main',
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
@@ -31,7 +31,7 @@ return { -- Highlight, edit, and navigate code
       'typescript',
       'vim',
       'vimdoc',
-      'yaml'
+      'yaml',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
