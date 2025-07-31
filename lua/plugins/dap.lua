c26d3a2fdfb3beb7dab85e0983d060b7ed4dc60e
Samuel Wall <me@samuelwall.uk>
2025-07-31 22:42:25 +0100

    feat(react-native-devtools): only enable if npx available

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,6 +8,10 @@ return {
   {
     'sjwall/react-native-devtools.nvim',
     dir = require('../utils/directory').exists_or_nil('/workspaces/react-native-devtools-nvim'),
+    cond = function()
+      success = pcall(vim.fn.system, { 'npx', '--help' })
+      return success and vim.v.shell_error == 0
+    end,
     build = 'chmod +x build.sh && ./build.sh',
     event = 'VeryLazy',
     dependencies = {
