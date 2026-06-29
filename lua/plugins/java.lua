ed05c79f43d3181d26f6c4574f8cee9c555e1fad
Samuel Wall <me@samuelwall.uk>
2026-06-29 19:59:16 +0000

    feat(java): check enabled in config to enable

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,17 +1,20 @@
 return {
   'nvim-java/nvim-java',
-  cond = function()
-    local scan = require('plenary.scandir')
-    local matches = scan.scan_dir(vim.loop.cwd(), {
-      search_pattern = '%.java$',
-      depth = 1e3,
-      hidden = false,
-      add_filter = function(entry)
-        -- ignore anything under node_modules
-        return not vim.startswith(entry, 'node_modules')
-      end,
-    })
-    return #matches > 0
+  cond = function(self)
+    if require('config.config_loader').is_enabled(self.name) then
+      local scan = require('plenary.scandir')
+      local matches = scan.scan_dir(vim.loop.cwd(), {
+        search_pattern = '%.java$',
+        depth = 1e3,
+        hidden = false,
+        add_filter = function(entry)
+          -- ignore anything under node_modules
+          return not vim.startswith(entry, 'node_modules')
+        end,
+      })
+      return #matches > 0
+    end
+    return false
   end,
   config = function()
     require('java').setup()
